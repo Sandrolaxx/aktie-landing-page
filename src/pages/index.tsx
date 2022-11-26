c4895c0e3d9739073cacdcb23c8f774c228833dd
Sandrolaxx <sandro--ramos@hotmail.com>
2022-11-25 22:32:46 -0300

    ft committ

---
+++ src/pages/index.tsx
@@ -0,0 +1,23 @@
+import Head from 'next/head'
+
+export default function Home() {
+  return (
+    <div>
+      <Head>
+        <title>AKTIE TECH - Tecnologia para seu negócio</title>
+        <meta name="description" content="Desenvolvimento de sites, sistemas e aplicativos sob demanda." />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <p>
+          Landing Page AKTIE
+        </p>
+      </main>
+
+      <footer>
+          Powered by AKTIE TECH
+      </footer>
+    </div>
+  )
+}
