7d8bb5525f8237775b950844f61ed13e4b6b86d3
Sandrolaxx <sandro--ramos@hotmail.com>
2022-11-29 22:30:18 -0300

    add logo svg

--- next.config.js
+++ next.config.js
@@ -1,7 +1,17 @@
 /** @type {import('next').NextConfig} */
+const withReactSvg = require("next-react-svg");
+const path = require("path");
 const nextConfig = {
-  reactStrictMode: true,
-  swcMinify: true,
-}
+    reactStrictMode: true,
+    swcMinify: true,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
+
+module.exports = withReactSvg({
+    include: path.resolve(__dirname, "src/assets/icons"),
+
+    webpack(config, options) {
+        return config;
+    },
+})
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,25 @@
 import Head from 'next/head'
+import AktieIcon from "../assets/icons/aktLogo.svg";
 
 export default function Home() {
-  return (
-    <div>
-      <Head>
-        <title>AKTIE TECH - Tecnologia de ponta para seu negócio</title>
-        <meta name="description" content="Desenvolvimento de sites, sistemas e aplicativos sob demanda." />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    return (
+        <div>
+            <Head>
+                <title>AKTIE TECH - Tecnologia de ponta para seu negócio</title>
+                <meta name="description" content="Desenvolvimento de sites, sistemas e aplicativos sob demanda." />
+                <link rel="icon" href="/favicon.ico" />
+            </Head>
 
-      <main className="w-full h-full flex items-center justify-center">
-        <p>
-          Content
-        </p>
-      </main>
+            <main className="w-full h-full flex items-center justify-center">
+                <p>
+                    Content
+                </p>
+                <AktieIcon />
+            </main>
 
-      <footer>
-          Powered by AKTIE TECH
-      </footer>
-    </div>
-  )
+            <footer>
+                Powered by AKTIE TECH
+            </footer>
+        </div>
+    )
 }
