dc98b246cac5a6462a759baaa62cf2849dab8283
Sandrolaxx <sandro--ramos@hotmail.com>
2022-12-02 07:49:43 -0300

    add button gradient animation and font

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,21 +8,39 @@ export default function Home() {
                 <title>AKTIE TECH - Tecnologia de ponta para seu negócio</title>
                 <meta name="description" content="Desenvolvimento de sites, sistemas e aplicativos sob demanda." />
                 <link rel="icon" href="/favicon.ico" />
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link rel="preconnect" href="https://fonts.gstatic.com" />
+                <link href="https://fonts.googleapis.com/css2?family=Work+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;1,300;1,400;1,500;1,600;1,700&display=swap" rel="stylesheet" />
             </Head>
 
-            <main className="h-full">
+            <main className="h-full font-work">
                 <Menu />
-                <div className="w-full h-2/3 flex justify-center bg-purple-100">
-                    <div className="w-1/2 p-32">
+                <div className="w-full h-2/3 flex justify-center">
+                    <div className="w-1/2 flex flex-col justify-center items-center">
                         <p className="text-3xl whitespace-pre-wrap">
-                            Tecnologia de ponta para sua empresa,{"\n"}
-                            de sistemas complexos até seu primeiro aplicativo
-                            podemos ajudar a agregar cada vez mais {"\n"}
-                            valor ao seu negócio
+                            <span className="text-primary">
+                                Tecnologia de ponta {""}
+                            </span>
+                            para sua empresa,{"\n"}
+                            de sistemas complexos até seu primeiro {"\n"}
+                            aplicativo podemos te ajudar a agregar {"\n"}
+                            cada vez mais {""}
+                            <span className="text-secondary">
+                                valor ao seu negócio.
+                            </span>
                         </p>
+                        <div className="my-6">
+                            <button className="w-40 h-14 mx-6 text-white font-medium text-lg border rounded-full
+                                bg-gradient-to-r from-primary via-red-500 to-secondary animate-gradient-x">
+                                Contate-nos
+                            </button>
+                            <button className="w-40 h-14 font-medium text-lg border rounded-full">
+                                Veja mais
+                            </button>
+                        </div>
                     </div>
-                    <div className="w-1/2 p-32">
-                        Teste
+                    <div className="w-1/2 flex flex-col justify-center items-center">
+                        FOTO
                     </div>
                 </div>
             </main>
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,46 @@ module.exports = {
                 "primary": "#FF5F05",
                 "secondary": "#9453E5",
             },
+            fontFamily: {
+                'work': ['Work Sans', 'sans-serif']
+            },
+            animation: {
+                'gradient-x':'gradient-x 10s ease infinite',
+                'gradient-y':'gradient-y 10s ease infinite',
+                'gradient-xy':'gradient-xy 10s ease infinite',
+            },
+            keyframes: {
+                'gradient-y': {
+                    '0%, 100%': {
+                        'background-size':'400% 400%',
+                        'background-position': 'center top'
+                    },
+                    '50%': {
+                        'background-size':'200% 200%',
+                        'background-position': 'center center'
+                    }
+                },
+                'gradient-x': {
+                    '0%, 100%': {
+                        'background-size':'200% 200%',
+                        'background-position': 'left center'
+                    },
+                    '50%': {
+                        'background-size':'200% 200%',
+                        'background-position': 'right center'
+                    }
+                },
+                'gradient-xy': {
+                    '0%, 100%': {
+                        'background-size':'400% 400%',
+                        'background-position': 'left center'
+                    },
+                    '50%': {
+                        'background-size':'200% 200%',
+                        'background-position': 'right center'
+                    }
+                }
+            }
         },
     },
     plugins: [],
