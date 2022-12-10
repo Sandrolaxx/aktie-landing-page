cbfc885654a189e860c399cc4845d7ba5d254a02
Sandrolaxx <sandro--ramos@hotmail.com>
2022-12-10 16:15:57 -0300

    add solution animation

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import { useEffect, useState } from "react";
-import animationData from "../assets/lottie/apps.json";
+import appsAnimation from "../assets/lottie/apps.json";
+import companyAnimation from "../assets/lottie/company.json";
 import Home from "../components/Home";
 import LottieElement from "../components/LottieElement";
 import Menu from "../components/Menu";
@@ -27,13 +28,14 @@ export default function Main() {
             <main className="h-full">
                 <Menu />
                 <Home />
-                <span className="w-full h-full flex flex-col items-center">
+
+                <div className="w-full h-full flex flex-col items-center">
                     <span className="w-3/4 h-24 pt-24 animate-fade-up">
                         <hr className="h-0.5 bg-primary dark:bg-secondary rounded-full" />
                     </span>
                     <div className="w-full h-full flex justify-center items-center">
                         <div className="mt-10 animate-fade-in">
-                            <LottieElement animationFile={animationData} />
+                            <LottieElement animationFile={appsAnimation} />
                         </div>
                         <span className="">
                             <p className="text-2xl md:whitespace-pre-wrap lg:text-3xl">
@@ -42,10 +44,29 @@ export default function Main() {
                             </p>
                         </span>
                     </div>
-                </span>
+                </div>
 
-                <div className="h-full">
-
+                <div className="w-full h-full flex flex-col items-center">
+                    <span className="w-3/4 h-24 pt-24">
+                        <hr className="h-0.5 bg-primary dark:bg-secondary rounded-full" />
+                    </span>
+                    <div className="w-full h-full flex justify-center items-center">
+                        <span className="">
+                            <p className="text-2xl md:whitespace-pre-wrap lg:text-3xl">
+                                Automatize os precessos e deixe{"\n"}
+                                seus colaboradores livres para atuarem {"\n"}
+                                em questões criativas e não operacionais.
+                            </p>
+                        </span>
+                        <div className="w-2/6 h-3/4">
+                            <LottieElement animationFile={companyAnimation}
+                                withActions actions={{
+                                    type: "seek",
+                                    frames: [0, 200],
+                                    visibility: [0, 1],
+                                }} />
+                        </div>
+                    </div>
                 </div>
 
                 <footer className="h-14 bottom-14 flex justify-center pt-4">
