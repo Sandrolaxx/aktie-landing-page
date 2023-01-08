2653f1c45c7d180985f32d7a57343222f7235931
Sandrolaxx <sandro--ramos@hotmail.com>
2023-01-07 23:20:14 -0300

    add form base

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -10,11 +10,34 @@ export default function Contact() {
                         lg:text-left lg:text-3xl py-10">
                     Contate-nos
                 </p>
-                <div className="w-full h-full flex justify-center">
-                    <p className="mt-4 text-sm font-medium text-center md:text-base">
-                        Desenvolver Form
-                    </p>
-                </div>
+                <form className="flex w-full max-w-sm space-x-3">
+                    <div className="w-full max-w-2xl px-5 py-10 m-auto mt-10 bg-white rounded-lg shadow dark:bg-gray-800">
+                        <div className="grid max-w-xl grid-cols-2 gap-4 m-auto">
+                            <div className="col-span-2 lg:col-span-1">
+                                <div className=" relative ">
+                                    <input type="text" id="contact-form-name" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="Name" />
+                                </div>
+                            </div>
+                            <div className="col-span-2 lg:col-span-1">
+                                <div className=" relative ">
+                                    <input type="text" id="contact-form-email" className=" rounded-lg border-transparent flex-1 appearance-none border border-gray-300 w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-sm text-base focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" placeholder="email" />
+                                </div>
+                            </div>
+                            <div className="col-span-2">
+                                <label className="text-gray-700" htmlFor="name">
+                                    <textarea className="flex-1 w-full px-4 py-2 text-base text-gray-700 placeholder-gray-400 bg-white border border-gray-300 rounded-lg appearance-none focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent" id="comment" placeholder="Enter your comment" name="comment" rows={5} cols={40}>
+                                    </textarea>
+                                </label>
+                            </div>
+                            <div className="col-span-2 text-right">
+                                <button type="submit" className="py-2 px-4  bg-indigo-600 hover:bg-indigo-700 focus:ring-indigo-500 focus:ring-offset-indigo-200 text-white w-full transition ease-in duration-200 text-center text-base font-semibold shadow-md focus:outline-none focus:ring-2 focus:ring-offset-2  rounded-lg ">
+                                    Send
+                                </button>
+                            </div>
+                        </div>
+                    </div>
+                </form>
+
             </div>
         </div >
     )
